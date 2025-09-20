f0e47f90330f7024f40d3670ae0175f0fb2439dc
N00byKing <N00byKing@hotmail.de>
2025-09-20 11:52:54 +0200

    Functional enough for sm64

--- Archipelago_DataStorageSim.cpp
+++ Archipelago_DataStorageSim.cpp
@@ -16,12 +16,12 @@ void WriteFileJSON(Json::Value root, std::string path);
 bool parse_response(std::string msg, std::string &request);
 
 void operation_replace(std::string key, Json::Value new_val) {
-    Json::Value store = sp_save_root["store"];
+    Json::Value& store = sp_save_root["store"];
     store[key] = new_val;
 }
 void operation_or(std::string key, Json::Value new_val) {
+    Json::Value& store = sp_save_root["store"];
     // Semi-broken implementation. Assumes integer or bool values atm
-    Json::Value store = sp_save_root["store"];
     if (store[key].isInt64()) {
         int64_t value = store[key].asInt64();
         store[key] = value | new_val.asInt64();
@@ -42,8 +42,8 @@ static std::map<const std::string , std::function<void(std::string,Json::Value)>
 };
 
 void resolveDataStorageOp(Json::Value dataOp) {
-    Json::Value store = sp_save_root["store"];
-    if (dataOp["cmd"] == "Set" || dataOp["cmd"] == "SetNotify") {
+    Json::Value& store = sp_save_root["store"];
+    if (dataOp["cmd"] == "Set") {
         std::string key = dataOp["key"].asString();
         Json::Value original_value = store.get(key, Json::nullValue);
         if (!store.get(key, Json::nullValue) && dataOp.get("default", Json::nullValue)) {
@@ -59,21 +59,21 @@ void resolveDataStorageOp(Json::Value dataOp) {
         if (dataOp["want_reply"].asBool()) {
             Json::Value fake_msg;
             std::string req;
-            fake_msg["cmd"] = "SetReply";
-            fake_msg["key"] = key;
-            fake_msg["value"] = store[key];
-            fake_msg["original_value"] = original_value;
-            fake_msg["slot"] = AP_OFFLINE_SLOT;
+            fake_msg[0]["cmd"] = "SetReply";
+            fake_msg[0]["key"] = key;
+            fake_msg[0]["value"] = store[key];
+            fake_msg[0]["original_value"] = original_value;
+            fake_msg[0]["slot"] = AP_OFFLINE_SLOT;
             parse_response(writer.write(fake_msg), req);
         }
     } else if (dataOp["cmd"] == "Get") {
         Json::Value fake_msg;
         std::string req;
-        fake_msg["cmd"] = "Retrieved";
-        fake_msg["keys"] = Json::objectValue;
+        fake_msg[0]["cmd"] = "Retrieved";
+        fake_msg[0]["keys"] = Json::objectValue;
         for (Json::Value key_v : dataOp["keys"]) {
             std::string key = key_v.asString();
-            fake_msg["keys"][key] = sp_save_root.get(key, Json::nullValue);
+            fake_msg[0]["keys"][key] = sp_save_root.get(key, Json::nullValue);
         }
         parse_response(writer.write(fake_msg), req);
     }
