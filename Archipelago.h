172f683d4306b4fa209949419993198f87d881ed
SilicDev <83308290+SilicDev@users.noreply.github.com>
2026-08-25 15:03:44 +0200

    Fix Bounce packets using string for slots (#46)
    * Fix Bounce packets using string for slots
    
    * Fix slots using UInt64 accessor

--- Archipelago.h
+++ Archipelago.h
@@ -216,7 +216,7 @@ struct AP_SetReply {
 
 struct AP_Bounce {
     std::vector<std::string>* games = nullptr; // Can be nullptr or empty, but must be set to either
-    std::vector<std::string>* slots = nullptr; // Can be nullptr or empty, but must be set to either
+    std::vector<int64_t>* slots = nullptr; // Can be nullptr or empty, but must be set to either
     std::vector<std::string>* tags  = nullptr; // Can be nullptr or empty, but must be set to either
     std::string data; // Valid JSON Data. Can also be primitive (Numbers or literals)
 };
